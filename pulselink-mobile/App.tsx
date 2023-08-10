670e4a381c1dfe3ef2835edfccfe5e546c458ffc
asgarovf <fasgarov2002@gmail.com>
2023-08-10 02:10:03 +0100

    [mobile]: refactor: permissions

--- pulselink-mobile/App.tsx
+++ pulselink-mobile/App.tsx
@@ -1,113 +1,34 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
-import AppleHealthKit, {
-  HealthKitPermissions,
-  HealthPermission,
-} from "react-native-health";
-import { RSA } from "react-native-rsa-native";
-import { Buffer } from "buffer";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as SplashScreen from "expo-splash-screen";
+import { usePoppins } from "styles/theme";
+import React from "react";
 
-let message =
-  "my secret messageaksofkoasfkoasfkoaskfokasfokoasfkoasfkoasfkoasfkokaosfkafskafskoasfko apsflpasflpasflplafsplpafslpafslpafslpafslasfkoasfkakosfkoasfkosfkoasfkoasfoasfkoafsokko";
-
-const PermissionsRef = AppleHealthKit.Constants.Permissions;
-
-/* Permission options */
-const permissions = {
-  permissions: {
-    read: [
-      PermissionsRef.ActiveEnergyBurned,
-      PermissionsRef.ActivitySummary,
-      PermissionsRef.AppleStandTime,
-      PermissionsRef.BloodType,
-      PermissionsRef.BodyFatPercentage,
-      PermissionsRef.BodyTemperature,
-      PermissionsRef.DateOfBirth,
-      PermissionsRef.DistanceWalkingRunning,
-      PermissionsRef.DistanceCycling,
-      PermissionsRef.DistanceSwimming,
-      PermissionsRef.EnergyConsumed,
-      PermissionsRef.Height,
-      PermissionsRef.Workout,
-      PermissionsRef.Weight,
-      PermissionsRef.Water,
-      PermissionsRef.Steps,
-      PermissionsRef.StepCount,
-      PermissionsRef.RunningSpeed,
-      PermissionsRef.HeartRate,
-      PermissionsRef.HeartRateVariability,
-      PermissionsRef.RestingHeartRate,
-      PermissionsRef.WalkingHeartRateAverage,
-    ],
-    write: [],
-  },
-} as HealthKitPermissions;
+const StackNavigator = createStackNavigator();
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const fontsLoaded = usePoppins();
+
+  React.useEffect(() => {
+    const hideSplashScreen = async (): Promise<void> => {
+      await SplashScreen.hideAsync();
+    };
+
+    if (fontsLoaded) {
+      hideSplashScreen();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Button
-        title="INIT"
-        onPress={() => {
-          AppleHealthKit.initHealthKit(permissions, (error: string, result) => {
-            /* Called after we receive a response from the system */
-
-            console.log(result);
-            if (error) {
-              console.log("[ERROR] Cannot grant permissions!");
-            }
-
-            /* Can now read or write to HealthKit */
-
-            const options = {
-              startDate: new Date(2020, 1, 1).toISOString(),
-            };
-
-            AppleHealthKit.getRestingHeartRate(
-              options,
-              (callbackError: string, result) => {
-                /* Samples are now collected from HealthKit */
-              }
-            );
-          });
-        }}
-      />
-      <Button
-        title="Status"
-        onPress={() => {
-          console.log(
-            AppleHealthKit.getAuthStatus(
-              permissions,
-              (callbackError: string, result) => {
-                console.log(callbackError, result);
-                /* Samples are now collected from HealthKit */
-              }
-            )
-          );
-        }}
-      />
-      <Button
-        title="Keys"
-        onPress={() => {
-          RSA.generateKeys(2048) // set key size
-            .then((keys) => {
-              // console.log("HERE", Buffer.from(keys.private, "hex"));
-              console.log("256 private:"); // the private key
-              console.log("256 public:", keys.public); // the public key
-              RSA.encrypt(message, keys.public).then((encodedMessage) => {
-                console.log(`the encoded message is ${encodedMessage}`);
-                console.log("HERE------");
-                RSA.decrypt(encodedMessage, keys.private).then(
-                  (decryptedMessage) => {
-                    console.log(`The original message was ${decryptedMessage}`);
-                  }
-                );
-              });
-            });
-        }}
-      />
     </View>
   );
 }
---
+++ pulselink-mobile/constants/permissions.ts
@@ -0,0 +1,75 @@
+import AppleHealthKit, { HealthKitPermissions } from "react-native-health";
+
+const PermissionsRef = AppleHealthKit.Constants.Permissions;
+
+export const permissions = {
+  permissions: {
+    read: [
+      PermissionsRef.ActiveEnergyBurned,
+      PermissionsRef.ActivitySummary,
+      PermissionsRef.AppleStandTime,
+      PermissionsRef.BloodType,
+      PermissionsRef.BodyFatPercentage,
+      PermissionsRef.BodyTemperature,
+      PermissionsRef.DateOfBirth,
+      PermissionsRef.DistanceWalkingRunning,
+      PermissionsRef.DistanceCycling,
+      PermissionsRef.DistanceSwimming,
+      PermissionsRef.EnergyConsumed,
+      PermissionsRef.Height,
+      PermissionsRef.Workout,
+      PermissionsRef.Weight,
+      PermissionsRef.Water,
+      PermissionsRef.Steps,
+      PermissionsRef.StepCount,
+      PermissionsRef.RunningSpeed,
+      PermissionsRef.HeartRate,
+      PermissionsRef.HeartRateVariability,
+      PermissionsRef.RestingHeartRate,
+      PermissionsRef.WalkingHeartRateAverage,
+    ],
+    write: [],
+  },
+} as HealthKitPermissions;
+
+// <Button
+// title="INIT"
+// onPress={() => {
+//   AppleHealthKit.initHealthKit(permissions, (error: string, result) => {
+//     /* Called after we receive a response from the system */
+
+//     console.log(result);
+//     if (error) {
+//       console.log("[ERROR] Cannot grant permissions!");
+//     }
+
+//     /* Can now read or write to HealthKit */
+
+//     const options = {
+//       startDate: new Date(2020, 1, 1).toISOString(),
+//     };
+
+//     AppleHealthKit.getRestingHeartRate(
+//       options,
+//       (callbackError: string, result) => {
+//         /* Samples are now collected from HealthKit */
+//       }
+//     );
+//   });
+// }}
+// />
+
+// <Button
+// title="Status"
+// onPress={() => {
+//   console.log(
+//     AppleHealthKit.getAuthStatus(
+//       permissions,
+//       (callbackError: string, result) => {
+//         console.log(callbackError, result);
+//         /* Samples are now collected from HealthKit */
+//       }
+//     )
+//   );
+// }}
+// />
